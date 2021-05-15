72a30cdf72d7e97a33a411662d65d587a75ca83c
Artemas-Muzanenhamo <artemas.muzanenhamo@rightmove.co.uk>
2021-05-15 09:55:57 +0100

    abstract swapping logic

--- artemas-java-algorithms/src/main/java/com/java/algorithms/artemas_java_algorithms/sort/BubbleSort.java
+++ artemas-java-algorithms/src/main/java/com/java/algorithms/artemas_java_algorithms/sort/BubbleSort.java
@@ -2,13 +2,19 @@ package com.java.algorithms.artemas_java_algorithms.sort;
 
 public class BubbleSort {
     public int[] sort(int[] input) {
+
+
         int firstNumber = input[0];
         int secondNumber = input[1];
 
         if (firstNumber > secondNumber) {
-            return new int[]{secondNumber, firstNumber};
+            return swapPair(firstNumber, secondNumber);
         }
 
         return input;
+    }
+
+    private int[] swapPair(int firstNumber, int secondNumber) {
+        return new int[]{secondNumber, firstNumber};
     }
 }
