5fd8dc1ddd866ffd304cb67bbd20d22b6e9fc46c
Artemas-Muzanenhamo <artemas.muzanenhamo@rightmove.co.uk>
2021-05-15 09:47:27 +0100

    make test pass

--- artemas-java-algorithms/src/main/java/com/java/algorithms/artemas_java_algorithms/sort/BubbleSort.java
+++ artemas-java-algorithms/src/main/java/com/java/algorithms/artemas_java_algorithms/sort/BubbleSort.java
@@ -2,6 +2,13 @@ package com.java.algorithms.artemas_java_algorithms.sort;
 
 public class BubbleSort {
     public int[] sort(int[] input) {
+        int firstNumber = input[0];
+        int secondNumber = input[1];
+
+        if (firstNumber > secondNumber) {
+            return new int[]{secondNumber, firstNumber};
+        }
+
         return new int[0];
     }
 }
